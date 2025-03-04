6b2204f34b52938a09a99a5cb9c118b98a3e9dc6
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2025-03-04 23:48:17 +0530

    implement user model

---
+++ src/main/java/com/rkisuru/blog/entity/User.java
@@ -0,0 +1,86 @@
+package com.rkisuru.blog.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.rkisuru.blog.type.RegistrationSource;
+import com.rkisuru.blog.type.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    private String sub;
+
+    @NotBlank
+    @Size(max = 20)
+    private String name;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @Size(max = 120)
+    @JsonIgnore
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private RegistrationSource registrationSource;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    private Set<Post> posts;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/rkisuru/blog/type/Role.java
@@ -0,0 +1,7 @@
+package com.rkisuru.blog.type;
+
+public enum Role {
+
+    ROLE_USER,
+    ROLE_ADMIN
+}
