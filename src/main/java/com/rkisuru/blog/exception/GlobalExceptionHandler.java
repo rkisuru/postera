f3a5b687b94956e62d2c04675524e49eb0025490
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2025-03-05 11:43:26 +0530

    add NotFoundException & refactor GlobalExceptionHandler

--- src/main/java/com/rkisuru/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/rkisuru/blog/exception/GlobalExceptionHandler.java
@@ -5,13 +5,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(OperationNotPermittedException.class)
     public ResponseEntity<ErrorResponse> operationNotPermittedException(OperationNotPermittedException e) {
 
-        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value());
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        ErrorResponse response = new ErrorResponse(e.getMessage(), HttpStatus.UNAUTHORIZED, LocalDateTime.now());
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> notFoundException(NotFoundException e) {
+
+        ErrorResponse response = new ErrorResponse(e.getMessage(), HttpStatus.NOT_FOUND, LocalDateTime.now());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 }
